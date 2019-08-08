a303ebb7aacf60688e61625212ebd0a3819e507f
drew6017 <drew6017@gmail.com>
2019-08-08 09:51:15 -0500

    Update FancyCosmeticType.java

--- src/fancy/cosmetics/FancyCosmeticType.java
+++ src/fancy/cosmetics/FancyCosmeticType.java
@@ -1,5 +1,6 @@
 package fancy.cosmetics;
 
+// TODO improve the inheritence here, e.g. make interfaces for each of these that extend the FancyCosmetic interface
 public enum FancyCosmeticType {
     PARTICLE, PET, GADGET;
 }
